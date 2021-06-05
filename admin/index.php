b9cc84e3b7a67aa002556bfb904dbae68c5c685d
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-06-05 10:37:32 +0430

    add create modal in admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -31,7 +31,7 @@
         <div class="card">
             <div class="card-header d-flex justify-content-between">
                 <h3 class="">Contacts</h3>
-                <button type="button" class="btn btn-success">button</button>
+                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">button</button>
             </div>
             <div class="card-body">
                 <table class="table table-dark table-striped">
@@ -58,6 +58,36 @@
                         </tr>
                     </tbody>
                 </table>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="" method="post">
+                    <div class="modal-body">
+
+                        <div class="form-group"><label for="name">name</label><input type="text" class="form-control" id="name"></div>
+                        <div class="form-group"><label for="last_name">last name</label><input type="text" class="form-control" id="last_name"></div>
+                        <div class="form-group"><label for="email">email</label><input type="email" class="form-control" id="email"></div>
+                        <div class="form-group"><label for="phone">phone</label><input type="text" class="form-control" id="phone"></div>
+
+
+                    </div>
+                    <div class="modal-footer">
+
+                        <button type="button" class="btn btn-success">button</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
