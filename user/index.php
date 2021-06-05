da5e3a18d7f4624922560c6776c28c1c2f6a09d8
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-06-05 10:55:37 +0430

    add user/index.php

---
+++ user/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+
+<body>
+
+
+
+
+
+    <script src="../js/jquery-3.6.0.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
