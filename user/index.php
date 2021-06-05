ec9a05c1c3867ca8ad053047829babfa3652e814
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-06-05 11:22:16 +0430

    add table of complaint to user/index.php

--- user/index.php
+++ user/index.php
@@ -29,6 +29,43 @@
 
 
 
+
+    <div class="container mt-3">
+        <div class="card">
+            <div class="card-header d-flex justify-content-between">
+                <h3 class="">complaint</h3>
+                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">button</button>
+            </div>
+            <div class="card-body" style="max-height:500px ;overflow:auto">
+                <table class="table  table-striped">
+                    <thead>
+
+                        <th>title</th>
+                        <th>answer</th>
+                        <th>actions</th>
+                    </thead>
+                    <tbody>
+                        <?php for($i=0;$i<10;$i++): ?>
+                        <tr>
+                            <td>withdraw</td>
+                            <td><a class="btn btn-outline-info" href="#" role="button">Link button</a></td>
+                            <td>
+                                <div class="btn-group btn-group-sm">
+                                    <button type="button" class="btn btn-outline-warning">edit</button>
+                                    <button type="button" class="btn btn-outline-danger">delete</button>
+
+                                </div>
+                            </td>
+                        </tr>
+                        <?php endfor;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
     <script src="../js/jquery-3.6.0.min.js"></script>
     <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
