32b55d71f00e546ee9903891c7954b594cd9d882
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-06-05 09:17:08 +0430

    initial commit with login form

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>hi</title>
+    <link rel='stylesheet' href="css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="card mt-5 shadow">
+            <div class="card-header">
+                <h3>login</h3>
+            </div>
+            <div class="card-body">
+                <form action="">
+                    <div class="form-group">
+                        <label for="username">username</label>
+                        <input type="email" class="form-control" placeholder="email address">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">password</label>
+                        <input type="password" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">login</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
